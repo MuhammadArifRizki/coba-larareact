ef9f8b758930f6f8ad2892469a9c03c92f67d1ff
Sego Udok <rarif7111@gmail.com>
2025-04-21 00:30:31 +0700

    v.1

--- coba-larareact/resources/js/Layouts/AuthenticatedLayout.jsx
+++ coba-larareact/resources/js/Layouts/AuthenticatedLayout.jsx
@@ -25,6 +25,8 @@ export default function Authenticated({ user, header, children }) {
                                     Dashboard
                                 </NavLink>
                             </div>
+
+                            
                         </div>
 
                         <div className="hidden sm:flex sm:items-center sm:ms-6">
@@ -91,11 +93,14 @@ export default function Authenticated({ user, header, children }) {
                 </div>
 
                 <div className={(showingNavigationDropdown ? 'block' : 'hidden') + ' sm:hidden'}>
+
+
                     <div className="pt-2 pb-3 space-y-1">
                         <ResponsiveNavLink href={route('dashboard')} active={route().current('dashboard')}>
                             Dashboard
                         </ResponsiveNavLink>
                     </div>
+
 
                     <div className="pt-4 pb-1 border-t border-gray-200">
                         <div className="px-4">
